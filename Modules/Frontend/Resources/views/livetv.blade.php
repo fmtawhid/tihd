7b539e628168e421abca09d0456ffec975d0b4a3
Tawhid Islam <79523960+fmtawhid@users.noreply.github.com>
2025-07-28 17:27:21 +0600

    update live tv ui

--- Modules/Frontend/Resources/views/livetv.blade.php
+++ Modules/Frontend/Resources/views/livetv.blade.php
@@ -35,7 +35,7 @@
         @include('frontend::components.section.livetvthumbnail',  ['livetvthumbnail' => $responseData['slider']])
     </div>
 
-<div id="comingsoon-card-list">
+    <div id="comingsoon-card-list">
         <div class="container-fluid">
             <div class="row gy-5 row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5" id="coming-soon">
             </div>
@@ -51,9 +51,9 @@
         </div>
     </div>
 
-		<div style="display: flex; justify-content: center;">
+		<!-- <div style="display: flex; justify-content: center;">
           <img src="https://tihd.tv/img/NoData.png" />
-		</div>
+		</div> -->
 
     <div class="container-fluid padding-right-0">
         <div class="overflow-hidden">
